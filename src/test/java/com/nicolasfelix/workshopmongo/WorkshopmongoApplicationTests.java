57dc6e514d57c31c955a67b61fa279b23405d47c
n4co <nf.oliveira@hotmail.com>
2021-10-19 20:59:56 -0300

    Projeto Criado

---
+++ src/test/java/com/nicolasfelix/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolasfelix.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
